1bb089b54ea34bad4a3e24596f6776400a7b8381
Gabriel-Bernedo <84144762+Darkfrost-1107@users.noreply.github.com>
2024-02-20 00:59:36 -0500

    starting routing

---
+++ my-app/src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Testing1 from '@/components/test/Testing1'
+
+export default function page() {
+  return (
+    <div>
+      Hello From About !!!
+      <Testing1 />      
+    </div>
+  )
+}
