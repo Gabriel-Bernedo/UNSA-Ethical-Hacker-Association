937ef364f195bff79a2e2f7dad2d10cdf4fafab3
Gabriel-Bernedo <84144762+Darkfrost-1107@users.noreply.github.com>
2024-02-20 01:37:12 -0500

    arrepentimiento x3

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA).
   distDir: '../docs', // Changes the build output directory to `./dist/`.
+  basePath: '/UNSA-Ethical-Hacker-Association',
 };
 
 export default nextConfig;
