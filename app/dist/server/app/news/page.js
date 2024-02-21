a0376cd78600ee5bfa7f633a53782a19f2194efc
Gabriel-Bernedo <84144762+Darkfrost-1107@users.noreply.github.com>
2024-02-21 09:47:24 -0500

    REMOVE :dist folder

--- app/dist/server/app/news/page.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/news/page.js";
-const runtime = require("../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_21b95f._.js");
-runtime.loadChunk("server/chunks/[root of the server]__54bf9a._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/news/page/actions.js (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-page.js/(COMPONENT_0)/[project]/src/app/layout.jsx [app-rsc] (ecmascript, Next.js server component)/(COMPONENT_1)/[project]/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)/(COMPONENT_2)/[project]/src/app/news/page.jsx [app-rsc] (ecmascript, Next.js server component) (ecmascript) {facade}", CHUNK_PUBLIC_PATH).exports;
--- app/dist/server/app/resources/page.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/resources/page.js";
-const runtime = require("../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_9d330c._.js");
-runtime.loadChunk("server/chunks/[root of the server]__3a190b._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/resources/page/actions.js (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-page.js/(COMPONENT_0)/[project]/src/app/layout.jsx [app-rsc] (ecmascript, Next.js server component)/(COMPONENT_1)/[project]/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)/(COMPONENT_2)/[project]/src/app/resources/page.jsx [app-rsc] (ecmascript, Next.js server component) (ecmascript) {facade}", CHUNK_PUBLIC_PATH).exports;
