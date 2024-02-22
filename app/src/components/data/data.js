991db425ebb9077e8a83452de011233809618aab
Gabriel-Bernedo <84144762+Darkfrost-1107@users.noreply.github.com>
2024-02-21 21:12:32 -0500

    Update data.js

--- app/src/components/data/data.js
+++ app/src/components/data/data.js
@@ -1,5 +1,5 @@
-import data from "./data.json"
+import database from "./data.json"
 
-export const index = data.index
-export const resources = data.resources
-export const data = data.data
+export const index = database.index
+export const resources = database.resources
+export const data = database.data
