f28ee9d2272662c57d72e34333519cfb4fcc9d0b
Gabriel-Bernedo <gbernedok@unsa.edu.pe>
2024-03-17 01:11:22 -0500

    xd

--- app/src/app/api/user/route.js
+++ app/src/app/api/user/route.js
@@ -7,33 +7,34 @@ export async function GET (req){
   let data
   console.log(searchParams)
   if(searchParams) {
-    const user = prisma.user.findUnique({
-      where: {
-        
-      }
-    })
+    const user = await prisma.user.findMany()
     data = await prisma.user.findMany()
   } else {
     data = {msg: "No tiene permisos suficientes"}
   }
-  prisma.$disconnect()
   return NextResponse.json(data);
 }
 
-export async function POST(req, data){
-  const params = data.params
-  const prisma = new PrismaClient()
-  const user = prisma.user.findUnique({
-    where: {
-      name: params.name,
-      password : params.password,
-    }
-  })
+export async function POST(req){
+  const {searchParams} = new URL(req.url)
+  console.log(req.body[0] == undefined)
+  console.log(await req.json?.())
+  // let data = await req.json()
+  // console.log(data)
+  return NextResponse.json({})
+  // const params = data.params
+  // const prisma = new PrismaClient()
+  // const user = prisma.user.findUnique({
+  //   where: {
+  //     name: params.name,
+  //     password : params.password,
+  //   }
+  // })
 
-  return  NextResponse.json(
-    {auth: {
-      message,
-      error,
-    }}
-  )
+  // return  NextResponse.json(
+  //   {auth: {
+  //     message,
+  //     error,
+  //   }}
+  // )
 }
